444f389478b10ea70601e2909b89224bbe0882ff
Filatei <filatei@gtsng.com>
2019-02-04 09:22:04 +0100

    changing landing page 2

--- 4b-login-playlist/routes/index.js
+++ 4b-login-playlist/routes/index.js
@@ -4,39 +4,31 @@ const fs = require("fs");
 const path = require("path");
 
 const subjectsData = require('../models/subject');
-const videosPath = path.join(__dirname, '../videos');
+// const videosPath = path.join(__dirname, '../videos');
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
-	// Function to list folders in videos dir
-	var folders = fs.readdirSync(videosPath);
-
-
-	// Each folder is a video category.
-	// for each category, get all the mp4 files
-	catObj = [];  //store all folders and their mp4files
-	let mtime = 0;
-	folders.forEach(function (folder,index){
-		fpath = path.join(__dirname, '../videos',folder);
-		
-		mp4files = fs.readdirSync(fpath);
-		let stats = fs.statSync(fpath);
-		mtime = new Date(stats.mtime);
-		let seconds = (new Date().getTime() - stats.mtime) / 1000;
-		let gid = stats.dev + stats.ino;
-		let days = (seconds/(60*60*24)).toFixed(0);
-		console.log(`modification time ${days} days`);	
-		catObj.push({category:folder,files:mp4files,mtime:days,gid:gid})
-	});
-
-  	
-	res.render('listcourses', { 
-	videoTitle: 'Dashboard',
-	videoGroup: catObj,
-	videoDir: 'Dashboard',
-	
-
-	});
+	let resultObj = {};
+    subjectsData.find( (err, doc)=>{
+        if (err) res.status(404).send('Error Encountered');
+        else if (doc) {
+            
+            // docj = JSON.stringify(doc);
+            resultObj = doc;
+        }
+    })
+    .then(() => {
+        
+	    res.render('listcourses', { 
+            title: 'List of Courses',
+            result: resultObj,
+            count: resultObj.length
+        });
+        
+    })
+    .catch((err) => {
+        res.status(500).send(err);
+    })
 });
 
 function ensureAuthenticated(req, res, next){
